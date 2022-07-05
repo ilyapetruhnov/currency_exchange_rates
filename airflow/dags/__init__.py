4d14086bea2110ed588345eac7d56b123cb66fd2
ilya <petrukha16@gmail.com>
2022-07-05 20:26:37 +0000

    set up

---
+++ airflow/dags/__init__.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from .operator import CurrencyScoopOperator
+
+
+with DAG(
+        dag_id='exchange_rate_usd_byn_dag',
+        start_date=datetime(2021, 3, 1),
+        schedule_interval='@daily',
+) as dag:
+
+    create_table = PostgresOperator(
+        task_id='create_table_task',
+        sql='sql/create_table.sql',
+        postgres_conn_id='postgres_default',
+    )
+
+    get_rate = CurrencyScoopOperator(
+        task_id='get_rate',
+        base_currency='USD',
+        currency='BYN',
+        conn_id='cur_scoop_conn_id',
+        dag=dag,
+        do_xcom_push=True,
+    )
+
+    insert_rate = PostgresOperator(
+        task_id='insert_rate',
+        postgres_conn_id='postgres_default',
+        sql='sql/insert_rate.sql',
+        params={
+            'base_currency': 'USD',
+            'currency': 'BYN',
+        }
+    )
+
+    create_table >> get_rate >> insert_rate
